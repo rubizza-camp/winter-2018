6c14520833e33ff74f273ede7f5d3a7a97ec7fc6
Sikorskyyy <sikorskyalexandr08@gmail.com>
2018-12-03 21:31:16 +0300

    fixed untracked files

--- lib/price_parser.rb
+++ lib/price_parser.rb
@@ -4,16 +4,19 @@ require 'roo-xls'
 require 'csv'
 
 def convert_excel2csv(excel_file, csv_file)
-  excel = if excel_file =~ /xlsx$/
-            Roo::Excelx.new(excel_file)
-          else
-            Roo::Excel.new(excel_file)
-          end
+  begin
+    excel = if excel_file =~ /xlsx$/
+              Roo::Excelx.new(excel_file)
+            else
+              Roo::Excel.new(excel_file)
+            end
 
-  output = File.open(csv_file, 'w')
-  7.upto(excel.last_row) do |line|
-    output.write CSV.generate_line excel.row(line)
+    output = File.open(csv_file, 'w')
+    7.upto(excel.last_row) do |line|
+      output.write CSV.generate_line excel.row(line)
+    end
+  rescue => exception
+    puts exception.message
   end
-rescue StandardError => exception
-  puts exception.message
 end
+
