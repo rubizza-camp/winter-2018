f1c869b11b956598995525308e12c0fdf0cd9eae
Яна Манцевич <36705460+yanamanc@users.noreply.github.com>
2018-12-05 00:59:20 +0200

    Update main.rb

--- 0 - Yana Mantsevich/main.rb
+++ 0 - Yana Mantsevich/main.rb
@@ -1,32 +1,35 @@
 require 'creek'
-
 class PriceStatistic
   def initialize
     @creek = Creek::Book.new 'сборка.xlsx'
     @average_in_mounth = Hash.new { |mounth, min_price| }
-    @product = set_product_name
+    @product
+    set_product_name
     @nowaday_price = 0
   end
 
   def set_product_name
     puts 'What price are you looking for?'
-    product = gets.chomp
+    @product = gets.chomp
   end
 
   def nowadays_product_price
     sum = 0.0
     kol = 0
     @creek.sheets.last.simple_rows.each do |row|
-      if row['A'] =~ /#{@product}/i
+      if /#{@product}/i.match(row['A'])
         sum += row['G']
         kol += 1
       end
     end
+    product_availability(kol, sum)
+  end
 
-    if kol == 0
+  def product_availability(kol, sum)
+    if kol.zero?
       puts "#{@product} can not be found in database."
     else
-      puts "#{@product} is #{@nowaday_price = sum / kol} BYN in Minsk these days"
+      puts "#{@product} is #{@nowaday_price = sum / kol} BYN in Minsk these day"
     end
   end
 
@@ -36,7 +39,7 @@ class PriceStatistic
       avg_price = 0
       kol = 0
       sheet.simple_rows.each do |row|
-        if row['A'] =~ /#{@product}/i
+        if /#{@product}/i.match(row['A'])
           avg_price += row['C'].to_f + row['D'].to_f + row['E'].to_f + row['F'].to_f + row['G'].to_f + row['I'].to_f
           kol += 6
         end
@@ -45,17 +48,23 @@ class PriceStatistic
       @average_in_mounth[mounth] = avg_price / kol
       mounth += 1
     end
-    puts "Lowest was on #{2009 + @average_in_mounth.key(@average_in_mounth.values.min) / 12}/#{@average_in_mounth.key(@average_in_mounth.values.min) % 12} at price  #{@average_in_mounth.values.min}"
-    puts "Maximum was on #{2009 + @average_in_mounth.key(@average_in_mounth.values.max) / 12}/#{@average_in_mounth.key(@average_in_mounth.values.max) % 12} at #{@average_in_mounth.values.max}"
+    min_max_output
+  end
+
+  def min_max_output
+    min = @average_in_mounth.key(@average_in_mounth.values.min)
+    max = @average_in_mounth.key(@average_in_mounth.values.max)
+    min_price = @average_in_mounth.values.min
+    max_price = @average_in_mounth.values.max
+    puts "Lowest was on #{2009 + min / 12}/#{min % 12} at price  #{min_price}"
+    puts "Maximum was on #{2009 + max / 12}/#{max % 12} at #{max_price}"
   end
 
   def products_for_similar_price
     cheep_products = []
-    kol = 0
-    sum = 0
-
     @creek.sheets.last.simple_rows.each do |row|
-      if row['A'] != ~ /@product/i && row['C'].to_f + row['D'].to_f + row['E'].to_f + row['F'].to_f + row['G'].to_f + row['I'].to_f < @nowaday_price * 6
+      sum = row['C'].to_f + row['D'].to_f + row['E'].to_f + row['F'].to_f + row['G'].to_f + row['I'].to_f
+      if row['A'] != ~ /@product/i && sum < @nowaday_price * 6
         cheep_products << row['A']
       end
     end
