1a7d59dfa652d34ce9f56ccac4f5be3103697e4d
Яна Манцевич <36705460+yanamanc@users.noreply.github.com>
2018-12-05 11:07:44 +0200

    Fix1

--- 0 - Yana Mantsevich/main.rb
+++ 0 - Yana Mantsevich/main.rb
@@ -1,9 +1,9 @@
 require 'creek'
+
 class PriceStatistic
   def initialize
     @creek = Creek::Book.new 'сборка.xlsx'
     @average_in_mounth = Hash.new { |mounth, min_price| }
-    @product
     set_product_name
     @nowaday_price = 0
   end
@@ -36,37 +36,37 @@ class PriceStatistic
   def min_and_max_product_prices
     mounth = 1
     @creek.sheets.each do |sheet|
-      avg_price = 0
-      kol = 0
       sheet.simple_rows.each do |row|
-        if /#{@product}/i.match(row['A'])
-          avg_price += row['C'].to_f + row['D'].to_f + row['E'].to_f + row['F'].to_f + row['G'].to_f + row['I'].to_f
-          kol += 6
-        end
+        count_total_price(row, mounth)
       end
-      kol *= 10_000 if mounth <= 96
-      @average_in_mounth[mounth] = avg_price / kol
       mounth += 1
     end
     min_max_output
   end
 
+  def count_total_price(row, mounth)
+    avg_price = 0
+    kol = 0
+    if /#{@product}/i.match(row['A'])
+      avg_price += row['C'].to_f + row['D'].to_f + row['E'].to_f + row['F'].to_f + row['G'].to_f + row['I'].to_f
+      kol += 6
+    end
+    kol *= 10_000 if mounth <= 96 && !kol.zero?
+    @average_in_mounth[mounth] = avg_price / kol if !kol.zero?
+  end
+
   def min_max_output
     min = @average_in_mounth.key(@average_in_mounth.values.min)
     max = @average_in_mounth.key(@average_in_mounth.values.max)
-    min_price = @average_in_mounth.values.min
-    max_price = @average_in_mounth.values.max
-    puts "Lowest was on #{2009 + min / 12}/#{min % 12} at price  #{min_price}"
-    puts "Maximum was on #{2009 + max / 12}/#{max % 12} at #{max_price}"
+    puts "Lowest was on #{2009 + min / 12}/#{min % 12} at price  #{@average_in_mounth.values.min}"
+    puts "Maximum was on #{2009 + max / 12}/#{max % 12} at #{@average_in_mounth.values.max}"
   end
 
   def products_for_similar_price
     cheep_products = []
     @creek.sheets.last.simple_rows.each do |row|
       sum = row['C'].to_f + row['D'].to_f + row['E'].to_f + row['F'].to_f + row['G'].to_f + row['I'].to_f
-      if row['A'] != ~ /@product/i && sum < @nowaday_price * 6
-        cheep_products << row['A']
-      end
+      cheep_products << row['A'] if row['A'] != ~ /@product/i && sum < @nowaday_price * 6
     end
     puts 'For similar price you also can afford:'
     puts cheep_products
