093a530aaba58e618305944d5690c3fd9ba43161
Pavel Zhachko <rammiel@yandex.ru>
2018-12-06 01:20:45 +0300

    first lvl done

--- Pavel Zhachko/0/parser.rb
+++ Pavel Zhachko/0/parser.rb
@@ -13,38 +13,33 @@
 
 require 'roo'
 # initialize in class
-xsl = Roo::Spreadsheet.open('data/Average_prices(serv)-10-2018.xlsx')
-puts 'What price are you looking for?'
-input = gets.chomp
-
-# for find or search method in class (Lvl 1 tast 25% done. I hope.)
-xsl.sheets.each do |sheet_name|
-  # puts ''
-  # puts sheet_name
-  # puts '--------------'
-  # xsl.each { |elem| puts elem[0] }
-  sheet = xsl.sheet(sheet_name)
-  result = []
-  unless sheet.nil?
-    last_row    = sheet.last_row
-    last_column = sheet.last_column
-
-    return "Seems no data in sheet: #{sheet_name}" if last_row.nil? && last_column.nil?
-
-    (1..last_row).each do |row|
-      (1..last_column).each do |col|
-        v = sheet.cell(row, col)
-        if sheet.cell(row, 1) =~ /^#{input.upcase}/ && !v.nil?
-          result <<
-            "#{sheet.cell(row, 1)} is #{sheet.cell(row, 15)} BYN in Minsk these days."
-        end
-        # if !v.nil?
-        #   puts "NIL"
-        # else
-        #   puts "["+row.to_s+","+col.to_s+"]: " + sheet.cell(row, col).to_s
-        # end
-      end
-    end
+class RooBookParser
+  def initialize
+    @xsl = Roo::Spreadsheet.open('data/Average_prices(serv)-10-2018.xlsx')
+    @result = []
   end
-  puts result.uniq!
+
+  def output_parser(prices)
+    # lvl 1
+    prices.each do |elem|
+      puts "#{elem[0]} is #{elem[14]} BYN in Minsk these days."
+    end
+    # lvl 2
+    # lvl 3
+  end
+
+  def test_search; end
+
+  def search
+    puts 'What price are you looking for?'
+    input = gets.chomp
+    @xsl.each do |elem|
+      @result << elem if elem[0] =~ /^#{input.upcase}\b/
+    end
+    puts "\'#{input}\' can not be found in database." if @result.empty?
+    output_parser(@result)
+  end
 end
+
+r = RooBookParser.new
+r.search
