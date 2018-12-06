8bb9acfe3b62582dc12b47a5f20651f92be06ada
Pavel Zhachko <rammiel@yandex.ru>
2018-12-06 23:27:09 +0300

    lvl 3 done. code is ugly and it will be refactoring soon

--- Pavel Zhachko/0/parser.rb
+++ Pavel Zhachko/0/parser.rb
@@ -17,6 +17,8 @@ class RooBookParser
   def initialize
     @xsl = Roo::Spreadsheet.open('data/Average_prices(serv)-10-2018.xlsx')
     @result = []
+    @similar_price = []
+    @similar_result = []
   end
 
   def output_parser(prices)
@@ -26,20 +28,56 @@ class RooBookParser
     end
     # lvl 2
     # lvl 3
+    prices.each do |elem2|
+    end
   end
 
   def test_search; end
 
-  def search
+  def search_similr_price(price, name)
+    @xsl.each do |elem2|
+      unless elem2[0] =~ /^#{name.upcase}\b/
+        price.each do |sim_price|
+          @similar_result << elem2[0]
+                             .split(' ')
+                             .select { |el| el =~ /[[:upper:]]+/ }
+                             .join(' ').downcase if elem2.include?(sim_price)
+        end
+      end
+    end
+    puts 'Similar prices can not be found in database.' if @similar_result.empty?
+    similar_output(@similar_result)
+  end
+
+  def similar_output(similar)
+    # p similar
+    # similar.map! do |s|
+    #   s.split(' ').select { |el| el =~ /[[:upper:]]+/ }.join(' ').downcase
+    # end
+    if similar.length == 2
+      puts "For similar price you also can afford \'#{similar[0]}\' and \'#{similar[1]}\'"
+    elsif similar.length >= 3
+      puts 'For similar price you also can afford \'' + similar[0..similar.length - 2].join('\', \'') +
+           + '\' and ' + "\'#{similar[-1]}\'"
+    else
+      puts 'Something wrong'
+    end
+  end
+
+  def search_price_by_name
     puts 'What price are you looking for?'
     input = gets.chomp
     @xsl.each do |elem|
-      @result << elem if elem[0] =~ /^#{input.upcase}\b/
+      if elem[0] =~ /^#{input.upcase}\b/
+        @result << elem
+        @similar_price << elem[14]
+      end
     end
     puts "\'#{input}\' can not be found in database." if @result.empty?
     output_parser(@result)
+    search_similr_price(@similar_price, input)
   end
 end
 
 r = RooBookParser.new
-r.search
+r.search_price_by_name
