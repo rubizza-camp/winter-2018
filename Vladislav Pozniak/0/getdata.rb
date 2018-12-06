43a0a4a6c7acdccd1386288a54d941dddf00cda9
Rewerson <vladislav.pozniak@gmail.com>
2018-12-06 19:08:32 +0300

    Small fixes

--- Vladislav Pozniak/0/getdata.rb
+++ Vladislav Pozniak/0/getdata.rb
@@ -8,9 +8,11 @@ require 'uri'
 months = { 'январь': '01', 'февраль': '02', 'март': '03', 'апрель': '04',
            'май': '05', 'июнь': '06', 'июль': '07', 'август': '08',
            'сентябрь': '09', 'октябрь': '10', 'ноябрь': '11', 'декабрь': '12' }
+
 page = Nokogiri::HTML(open('http://www.belstat.gov.by/ofitsialnaya-statistika/makroekonomika-i-okruzhayushchaya-sreda/tseny/operativnaya-informatsiya_4/srednie-tseny-na-potrebitelskie-tovary-i-uslugi-po-respublike-belarus/'))
 links = page.css('a[href*="xls"]').map { |a| a['href'] }
 
+Dir.mkdir('data')
 links.each do |link|
   link.prepend('http://www.belstat.gov.by') if link[0] == '/'
   link = Addressable::URI.escape(link) unless link.include? '%'
@@ -19,7 +21,6 @@ links.each do |link|
   IO.copy_stream(URI.open(link), file)
   table = Roo::Spreadsheet.open(file, extension: File.extname(file))
   year = table.cell(3, 'A').split(' ')[-2]
-  month = months[table.cell(3, 'A').split(' ')[-3].to_sym]
-  Dir.mkdir 'data'
+  month = months[table.cell(3, 'A').split(' ')[-3].to_sym
   File.rename(file, ('./data/' + year + '.' + month + File.extname(file)))
 end
