825c0902416de3b3bb0cf501653c4e1e6d50d3f2
Valentin <kopotchel@outlook.com>
2018-12-07 09:57:41 +0300

    Valentine Kopotchel - 0_1

--- Valentine Kopotchel/0/bread_bot.rb
+++ Valentine Kopotchel/0/bread_bot.rb
@@ -5,131 +5,166 @@ rescue LoadError
   puts "Use 'bundle install --system' to download missing gems"
   exit
 end
+# some monkey patching
+class Regexp
+  def self.and_union(*res)
+    new res.flatten.map(&:source).join
+  end
+end
+HASH_OF_TABLES = {
+  '2009-01' => 'data/prices_tov_0109.xls',
+  '2009-02' => 'data/prices_tov_0209.xls',
+  '2009-03' => 'data/prices_tov_0309.xls',
+  '2009-04' => 'data/prices_tov_0409.xls',
+  '2009-05' => 'data/prices_tov_0509.xls',
+  '2009-06' => 'data/prices_tov_0609.xls',
+  '2009-07' => 'data/prices_tov_0709.xls',
+  '2009-08' => 'data/prices_tov_0809.xls',
+  '2009-09' => 'data/prices_tov_0909.xls',
+  '2009-10' => 'data/prices_tov_1009.xls',
+  '2009-11' => 'data/prices_tov_1109.xls',
+  '2009-12' => 'data/prices_tov_1209.xls',
+  '2010-01' => 'data/prices_tov_0110.xls',
+  '2010-02' => 'data/prices_tov_0210.xls',
+  '2010-03' => 'data/prices_tov_0310.xls',
+  '2010-04' => 'data/prices_tov_0410.xls',
+  '2010-05' => 'data/prices_tov_0510.xls',
+  '2010-06' => 'data/prices_tov_0610.xls',
+  '2010-07' => 'data/prices_tov_0710.xls',
+  '2010-08' => 'data/prices_tov_0810.xls',
+  '2010-09' => 'data/prices_tov_0910.xls',
+  '2010-10' => 'data/prices_tov_1010.xls',
+  '2010-11' => 'data/prices_tov_1110.xls',
+  '2010-12' => 'data/prices_tov_1210.xls',
+  '2011-01' => 'data/prices_tov_0111.xls',
+  '2011-02' => 'data/prices_tov_0211.xls',
+  '2011-03' => 'data/prices_tov_0311.xls',
+  '2011-04' => 'data/prices_tov_0411.xls',
+  '2011-05' => 'data/prices_tov_0511.xls',
+  '2011-06' => 'data/prices_tov_0611.xls',
+  '2011-07' => 'data/prices_tov_0711.xls',
+  '2011-08' => 'data/prices_tov_0811.xls',
+  '2011-09' => 'data/prices_tov_0911.xls',
+  '2011-10' => 'data/prices_tov_1011.xls',
+  '2011-11' => 'data/prices_tov_1111.xls',
+  '2011-12' => 'data/prices_tov_1211.xls',
+  '2012-01' => 'data/prices_tov_0112.xls',
+  '2012-02' => 'data/prices_tov_0212.xls',
+  '2012-03' => 'data/prices_tov_0312.xls',
+  '2012-04' => 'data/prices_tov_0412.xls',
+  '2012-05' => 'data/prices_tov_0512.xls',
+  '2012-06' => 'data/prices_tov_0612.xls',
+  '2012-07' => 'data/prices_tov_0712.xls',
+  '2012-08' => 'data/prices_tov_0812.xls',
+  '2012-09' => 'data/prices_tov_0912.xls',
+  '2012-10' => 'data/prices_tov_1012.xls',
+  '2012-11' => 'data/prices_tov_1112.xls',
+  '2012-12' => 'data/prices_tov_1212.xls',
+  '2013-01' => 'data/prices_tov_0113.xls',
+  '2013-02' => 'data/prices_tov_0213.xls',
+  '2013-03' => 'data/prices_tov_0313.xls',
+  '2013-04' => 'data/prices_tov_0413.xls',
+  '2013-05' => 'data/prices_tov_0513.xls',
+  '2013-06' => 'data/prices_tov_0613.xls',
+  '2013-07' => 'data/prices_tov_0713.xls',
+  '2013-08' => 'data/prices_tov_0813.xls',
+  '2013-09' => 'data/average_prices_goods_0913.xls',
+  '2013-10' => 'data/prices_1013.xls',
+  '2013-11' => 'data/prices_1113new.xls',
+  '2013-12' => 'data/prices_tov_1213.xls',
+  '2014-01' => 'data/prices_tov_0114.xls',
+  '2014-02' => 'data/prices_tov_0214_(1).xls',
+  '2014-03' => 'data/prices_tov_0314.xls',
+  '2014-04' => 'data/prices_tov_0414.xls',
+  '2014-05' => 'data/prices_tov_0514.xls',
+  '2014-06' => 'data/prices_tov_0614.xls',
+  '2014-07' => 'data/prices_tov_0714.xls',
+  '2014-08' => 'data/prices_tov_0814.xls',
+  '2014-09' => 'data/prices_tov_0914.xls',
+  '2014-10' => 'data/prices_tov_1014.xls',
+  '2014-11' => 'data/prices_tov_1114.xls',
+  '2014-12' => 'data/prises_cpi_12.xls',
+  '2015-01' => 'data/prices_tov_01_2015.xls',
+  '2015-02' => 'data/prices_tov_02_2015.xls',
+  '2015-03' => 'data/srednie_ceny_tovary_03_2015.xls',
+  '2015-04' => 'data/prices_tov_04_2015.xls',
+  '2015-05' => 'data/prices_tov05_2015.xls',
+  '2015-06' => 'data/ceny_06_2015_internet.xls',
+  '2015-07' => 'data/prices_tov_07_2015.xls',
+  '2015-08' => 'data/prices_tov_08_2015.xls',
+  '2015-09' => 'data/ceny_09_2015_internet.xlsx',
+  '2015-10' => 'data/prices_10_2015_goods.xlsx',
+  '2015-11' => 'data/Average prices_tov_11_2015_17_02.xlsx',
+  '2015-12' => 'data/prices_goods_2-2015_16_02.xlsx',
+  '2016-01' => 'data/prices_tov-01-2016.xlsx',
+  '2016-02' => 'data/Average pr_tov-02-2016.xlsx',
+  '2016-03' => 'data/Average prices_goods_03_2016.xlsx',
+  '2016-04' => 'data/prices_tov-04-2016.xlsx',
+  '2016-05' => 'data/цены-05-201610_06_2016.xlsx',
+  '2016-06' => 'data/price-06-2016.xlsx',
+  '2016-07' => 'data/price-07-2016.xlsx',
+  '2016-08' => 'data/Average prices _goods-08-2016.xlsx',
+  '2016-09' => 'data/Average_prices_goods-09-2016.xlsx',
+  '2016-10' => 'data/Prices_goods_10_2016.xlsx',
+  '2016-11' => 'data/Average prices_tov_11_2016.xlsx',
+  '2016-12' => 'data/prices_tov-12-2016.xlsx',
+  '2017-01' => 'data/price-01-2017.xlsx',
+  '2017-02' => 'data/Average_prices_02-2017.xlsx',
+  '2017-03' => 'data/Average_prices(serv)-03-2017.xlsx',
+  '2017-04' => 'data/Average_prices(serv)-04-2017.xlsx',
+  '2017-05' => 'data/Average prices(serv)-05-2017.xlsx',
+  '2017-06' => 'data/Average_prices(serv)-06-2017.xls',
+  '2017-07' => 'data/Average_prices(serv)-07-2017-2.xlsx',
+  '2017-08' => 'data/Average_prices(serv)-08-2017.xlsx',
+  '2017-09' => 'data/Average_prices(serv)-09-2017.xls',
+  '2017-10' => 'data/Average_prices(serv)-10-2017.xlsx',
+  '2017-11' => 'data/Average prices(serv)-07-2018.xlsx',
+  '2017-12' => 'data/Average prices(serv)-12-2017.xlsx',
+  '2018-01' => 'data/Average_prices(serv)-01-2018.xlsx',
+  '2018-02' => 'data/Average_prices(serv)-02-2018.xlsx',
+  '2018-03' => 'data/Average_prices(serv)-03-2018.xlsx',
+  '2018-04' => 'data/Average_prices(serv)-04-2018.xlsx',
+  '2018-05' => 'data/Average prices(serv)-05-2018.xlsx',
+  '2018-06' => 'data/Average_prices(serv)-06-2018.xlsx',
+  '2018-07' => 'data/Average prices(serv)-07-2018.xlsx',
+  '2018-08' => 'data/Average_prices(serv)-08-2018.xlsx',
+  '2018-09' => 'data/Average_prices(serv)-09-2018.xlsx',
+  '2018-10' => 'data/Average_prices(serv)-10-2018.xlsx'
+}.freeze
+# Doing some dirty work to make less code in BreadBot
 class BotHelper
-  HASH_OF_TABLES = {
-    '22016' => 'data/Average pr_tov-02-2016.xlsx',
-    '52017' => 'data/Average prices(serv)-05-2017.xlsx',
-    '72018' => 'data/Average prices(serv)-07-2018.xlsx',
-    '112017' => 'data/Average prices(serv)-07-2018.xlsx',
-    '52018' => 'data/Average prices(serv)-05-2018.xlsx',
-    '122017' => 'data/Average prices(serv)-12-2017.xlsx',
-    '82016' => 'data/Average prices _goods-08-2016.xlsx',
-    '32016' => 'data/Average prices_goods_03_2016.xlsx',
-    '112015' => 'data/Average prices_tov_11_2015_17_02.xlsx',
-    '112016' => 'data/Average prices_tov_11_2016.xlsx',
-    '12018' => 'data/Average_prices(serv)-01-2018.xlsx',
-    '22018' => 'data/Average_prices(serv)-02-2018.xlsx',
-    '32017' => 'data/Average_prices(serv)-03-2017.xlsx',
-    '32018' => 'data/Average_prices(serv)-03-2018.xlsx',
-    '42017' => 'data/Average_prices(serv)-04-2017.xlsx',
-    '42018' => 'data/Average_prices(serv)-04-2018.xlsx',
-    '62017' => 'data/Average_prices(serv)-06-2017.xls',
-    '62018' => 'data/Average_prices(serv)-06-2018.xlsx',
-    '72017' => 'data/Average_prices(serv)-07-2017-2.xlsx',
-    '82017' => 'data/Average_prices(serv)-08-2017.xlsx',
-    '82018' => 'data/Average_prices(serv)-08-2018.xlsx',
-    '92017' => 'data/Average_prices(serv)-09-2017.xls',
-    '92018' => 'data/Average_prices(serv)-09-2018.xlsx',
-    '102017' => 'data/Average_prices(serv)-10-2017.xlsx',
-    '102018' => 'data/Average_prices(serv)-10-2018.xlsx',
-    '22017' => 'data/Average_prices_02-2017.xlsx',
-    '92016' => 'data/Average_prices_goods-09-2016.xlsx',
-    '92013' => 'data/average_prices_goods_0913.xls',
-    '62015' => 'data/ceny_06_2015_internet.xls',
-    '92015' => 'data/ceny_09_2015_internet.xlsx',
-    '12017' => 'data/price-01-2017.xlsx',
-    '62016' => 'data/price-06-2016.xlsx',
-    '72016' => 'data/price-07-2016.xlsx',
-    '102015' => 'data/prices_10_2015_goods.xlsx',
-    '102013' => 'data/prices_1013.xls',
-    '112013' => 'data/prices_1113new.xls',
-    '122015' => 'data/prices_goods_2-2015_16_02.xlsx',
-    '102016' => 'data/Prices_goods_10_2016.xlsx',
-    '52015' => 'data/prices_tov05_2015.xls',
-    '12016' => 'data/prices_tov-01-2016.xlsx',
-    '42016' => 'data/prices_tov-04-2016.xlsx',
-    '122016' => 'data/prices_tov-12-2016.xlsx',
-    '12015' => 'data/prices_tov_01_2015.xls',
-    '22015' => 'data/prices_tov_02_2015.xls',
-    '42015' => 'data/prices_tov_04_2015.xls',
-    '72015' => 'data/prices_tov_07_2015.xls',
-    '82015' => 'data/prices_tov_08_2015.xls',
-    '12009' => 'data/prices_tov_0109.xls',
-    '12010' => 'data/prices_tov_0110.xls',
-    '12011' => 'data/prices_tov_0111.xls',
-    '12012' => 'data/prices_tov_0112.xls',
-    '12013' => 'data/prices_tov_0113.xls',
-    '12014' => 'data/prices_tov_0114.xls',
-    '22009' => 'data/prices_tov_0209.xls',
-    '22010' => 'data/prices_tov_0210.xls',
-    '22011' => 'data/prices_tov_0211.xls',
-    '22012' => 'data/prices_tov_0212.xls',
-    '22013' => 'data/prices_tov_0213.xls',
-    '22014' => 'data/prices_tov_0214_(1).xls',
-    '32009' => 'data/prices_tov_0309.xls',
-    '32010' => 'data/prices_tov_0310.xls',
-    '32011' => 'data/prices_tov_0311.xls',
-    '32012' => 'data/prices_tov_0312.xls',
-    '32013' => 'data/prices_tov_0313.xls',
-    '32014' => 'data/prices_tov_0314.xls',
-    '42009' => 'data/prices_tov_0409.xls',
-    '42010' => 'data/prices_tov_0410.xls',
-    '42011' => 'data/prices_tov_0411.xls',
-    '42012' => 'data/prices_tov_0412.xls',
-    '42013' => 'data/prices_tov_0413.xls',
-    '42014' => 'data/prices_tov_0414.xls',
-    '52009' => 'data/prices_tov_0509.xls',
-    '52010' => 'data/prices_tov_0510.xls',
-    '52011' => 'data/prices_tov_0511.xls',
-    '52012' => 'data/prices_tov_0512.xls',
-    '52013' => 'data/prices_tov_0513.xls',
-    '52014' => 'data/prices_tov_0514.xls',
-    '62009' => 'data/prices_tov_0609.xls',
-    '62010' => 'data/prices_tov_0610.xls',
-    '62011' => 'data/prices_tov_0611.xls',
-    '62013' => 'data/prices_tov_0613.xls',
-    '62012' => 'data/prices_tov_0612.xls',
-    '62014' => 'data/prices_tov_0614.xls',
-    '72009' => 'data/prices_tov_0709.xls',
-    '72010' => 'data/prices_tov_0710.xls',
-    '72011' => 'data/prices_tov_0711.xls',
-    '72012' => 'data/prices_tov_0712.xls',
-    '72013' => 'data/prices_tov_0713.xls',
-    '72014' => 'data/prices_tov_0714.xls',
-    '82009' => 'data/prices_tov_0809.xls',
-    '82010' => 'data/prices_tov_0810.xls',
-    '82011' => 'data/prices_tov_0811.xls',
-    '82012' => 'data/prices_tov_0812.xls',
-    '82013' => 'data/prices_tov_0813.xls',
-    '82014' => 'data/prices_tov_0814.xls',
-    '92009' => 'data/prices_tov_0909.xls',
-    '92010' => 'data/prices_tov_0910.xls',
-    '92011' => 'data/prices_tov_0911.xls',
-    '92012' => 'data/prices_tov_0912.xls',
-    '102009' => 'data/prices_tov_1009.xls',
-    '92014' => 'data/prices_tov_0914.xls',
-    '102010' => 'data/prices_tov_1010.xls',
-    '102011' => 'data/prices_tov_1011.xls',
-    '102012' => 'data/prices_tov_1012.xls',
-    '102014' => 'data/prices_tov_1014.xls',
-    '52016' => 'data/цены-05-201610_06_2016.xlsx',
-    '32015' => 'data/srednie_ceny_tovary_03_2015.xls',
-    '122014' => 'data/prises_cpi_12.xls',
-    '122013' => 'data/prices_tov_1213.xls',
-    '122012' => 'data/prices_tov_1212.xls',
-    '122011' => 'data/prices_tov_1211.xls',
-    '122010' => 'data/prices_tov_1210.xls',
-    '122009' => 'data/prices_tov_1209.xls',
-    '112014' => 'data/prices_tov_1114.xls',
-    '112011' => 'data/prices_tov_1111.xls',
-    '112012' => 'data/prices_tov_1112.xls',
-    '112010' => 'data/prices_tov_1110.xls',
-    '112009' => 'data/prices_tov_1109.xls'
-  }.freeze
-  def build_page(month, year)
-    path = './' + HASH_OF_TABLES[month.to_s + year.to_s]
+  PRICE_COLUMN_MINSK = 16
+  NAME_COLUMN = 0
+
+  def build_page(key)
+    path = HASH_OF_TABLES[key]
     xlsx = Roo::Spreadsheet.open(path)
     xlsx.sheet 0
+  end
+
+  def deflation?(date)
+    date[0, 4] < '2017'
+  end
+
+  def exists?(arg)
+    arg.to_s.length.positive?
+  end
+
+  def num?(arg)
+    arg.is_a? Numeric
+  end
+
+  def build_hash(key)
+    fin_hash = {}
+    condition = deflation? key
+    build_page(key).each do |row|
+      next unless exists?(row[NAME_COLUMN]) && num?(row[PRICE_COLUMN_MINSK])
+
+      val = row[PRICE_COLUMN_MINSK]
+      val /= 10_000.0 if condition
+      fin_hash[row[NAME_COLUMN]] = val
+    end
+    fin_hash
   end
 
   def make_normal(str)
@@ -137,40 +172,41 @@ class BotHelper
   end
 
   def reg_build(str)
+    str = str.strip
     r = str.upcase
-    r = Regexp.new("#{r}$|#{r}\s")
-    r
+    temp = Regexp.new("(#{r}$|#{r}\s|#{r},)")
+    Regexp.and_union(/(^|[\P{L}])/, temp)
   end
 
-  def extremum_change(val, hash, cur_m, cur_y, firstval = 0)
-    if val < hash['min'] && firstval > -1
-      hash['min'] = val
-      hash['min_year'] = cur_y
-      hash['min_month'] = cur_m
-    end
-    if val > hash['max']
-      hash['max'] = val
-      hash['max_year'] = cur_y
-      hash['max_month'] = cur_m
-    end
-    hash
+  def split_key(str)
+    [str[0, 4], str[-2, 2]]
+  end
+
+  def minmax_assign(hash)
+    min = hash.keys.min
+    max = hash.keys.max
+    a = hash[min]
+    b = hash[max]
+    [min, max, a, b]
+  end
+
+  def write_minmax_info(hash)
+    min, max, a, b = minmax_assign(hash)
+    puts "Lowest was on #{(split_key a)[1]}/#{(split_key a)[0]}"\
+"at #{min.round(2)} BYN"
+    puts "Highest was on #{(split_key b)[1]}/#{(split_key b)[0]}"\
+"at #{max.round(2)} BYN"
   end
 end
+# The main class
 class BreadBot
-  PRICE_COLUMN_MINSK = 16
-  NAME_COLUMN = 0
-  CURRENT_YEAR = 2018
-  CURRENT_MONTH = 10
-  BEGIN_YEAR = 2009
-
-  def initialize(month = CURRENT_MONTH, year = CURRENT_YEAR)
-    @h = {}
-    page = BotHelper.new.build_page(month, year)
-    page.each do |row|
-      if row[NAME_COLUMN].to_s.length.positive? && row[PRICE_COLUMN_MINSK].is_a?(Numeric)
-        @h[row[NAME_COLUMN]] = row[PRICE_COLUMN_MINSK]
-      end
+  def initialize
+    @big_hash = {}
+    dog = BotHelper.new
+    HASH_OF_TABLES.each_key do |key|
+      @big_hash[key] = dog.build_hash key
     end
+    @h = @big_hash[@big_hash.keys.max]
   end
 
   def gen_string(key)
@@ -178,38 +214,28 @@ class BreadBot
   end
 
   def count(key, str)
-    max = min = @h[key]
-    max_month = min_month = CURRENT_MONTH
-    max_year = min_year = CURRENT_YEAR
-    dog = BotHelper.new
-    hash_extr = { 'minmonth' => min_month, 'minyear' => min_year, 'min' => min,
-                  'maxmonth' => max_month, 'maxyear' => max_year, 'max' => max }
-    (BEGIN_YEAR...CURRENT_YEAR).each do |cur_y|
-      (1..12).each do |cur_m|
-        val = get_price_by_date(key, cur_m, cur_y, str)
-        firstval = val
-        val = val.to_f / 10_000.0 if cur_y.to_i < 2017
-        dog.extremum_change(val, hash_extr, cur_m, cur_y, firstval)
-      end
-      (1..CURRENT_MONTH).each do |cur_m|
-        val = get_price_by_date(key, cur_m, CURRENT_YEAR, str)
-        dog.extremum_change(val, hash_extr, cur_m, cur_y)
-      end
+    h = {}
+    @big_hash.each_key do |date|
+      val = get_price_by_date(date, key, str)
+      h[date] = val if val > -1
     end
-    hash_extr
+    min = h.values.min
+    max = h.values.max
+    { min => h.key(min), max => h.key(max) }
   end
 
-  def get_price_by_date(key, month, year, str)
-    b = BreadBot.new(month, year)
-    dog = BotHelper.new
-    return b.hash[key] if b.hash.key? key
-    return b.hash[b.find_match(dog.reg_build(str), b.hash)[0]] if b.find_match(dog.reg_build(str), b.hash).size == 1
+  def get_price_by_date(date, key, str)
+    work_hash = hash[date]
+    return work_hash[key] if work_hash.key? key
+
+    a = find_match(BotHelper.new.reg_build(str), work_hash)
+    return work_hash[a[0]] if a.size == 1
 
     -1
   end
 
   def hash
-    @h
+    @big_hash
   end
 
   def find_match(reg, hash = @h)
@@ -220,41 +246,73 @@ class BreadBot
     a
   end
 
-  def run
+  def prepare_ind(inv_hash, key)
+    temp_arr = inv_hash.to_a.sort
+    x, _y = temp_arr.transpose
+    [x.index(@h[key]), x]
+  end
+
+  def get_indexes(inv_hash, key)
+    ind, x = prepare_ind(inv_hash, key)
+    indexes = if ind.zero?
+                [1, 2]
+              elsif ind == x.size - 1
+                [ind - 1, ind - 2]
+              else
+                [ind - 1, ind + 1]
+              end
+    [indexes, x]
+  end
+
+  def level_2_string_generator(inv_hash, arr_of_num, keys)
     dog = BotHelper.new
+    puts 'For similar price you also can afford '\
+    "#{dog.make_normal inv_hash[arr_of_num[keys[0]]]}"\
+              " and #{dog.make_normal inv_hash[arr_of_num[keys[1]]]}"
+  end
+
+  def level_2_and_3(key, str)
+    puts "#{str} #{gen_string(key)}"
+    inv_hash = @h.invert
+    keys, x = get_indexes(inv_hash, key)
+    level_2_string_generator(inv_hash, x, keys)
+    level_3(key, str)
+  end
+
+  def level_3(key, str)
+    min_info = count(key, str)
+    BotHelper.new.write_minmax_info(min_info)
+  end
+
+  def some_preparations
+    puts 'What price are you looking for?'
+    str = gets.strip
+    r = BotHelper.new.reg_build(str)
+    [find_match(r), str]
+  end
+
+  def many_keys(arr)
+    puts "I can't understand what do you mean:( So here are some prices you"\
+'might be interested in:'
+    arr.each do |key|
+      puts BotHelper.new.make_normal(key) + ' ' + gen_string(key)
+    end
+  end
+
+  def loop_body(arr, str)
+    if arr.size.zero?
+      puts str + ' can not be found in database'
+    elsif arr.size == 1
+      level_2_and_3(arr[0], str)
+    else
+      many_keys(arr)
+    end
+  end
+
+  def run
     loop do
-      puts 'What price are you looking for?'
-      str = gets
-      str.rstrip!
-      str.lstrip!
-      r = dog.reg_build(str)
-      a = find_match(r)
-      if a.size.zero?
-        puts str + ' can not be found in database'
-      elsif a.size == 1
-        puts str + gen_string(a[0])
-        inv_hash = @h.invert
-        temp_arr = inv_hash.to_a.sort
-        x, _y = temp_arr.transpose
-        ind = x.index(@h[a[0]])
-        indexes = if ind.zero?
-                    [1, 2]
-                  else
-                    [ind - 1, ind + 1]
-                  end
-        key1, key2 = indexes
-        puts "For similar price you also can afford #{dog.make_normal inv_hash[x[key1]]}
-              and #{dog.make_normal inv_hash[x[key2]]}"
-        puts 'Wait a little for price-change dynamics...'
-        min_info = count(a[0], str)
-        puts "Lowest was on #{min_info['minmonth']}/#{min_info['minyear']} at #{min_info['min'].round(2)} BYN"
-        puts "Highest was on #{min_info['maxmonth']}/#{min_info['maxyear']} at #{min_info['max'].round(2)} BYN"
-      else
-        puts "I can't understand what do you mean:( So here are some prices you might be interested in:"
-        a.each do |key|
-          puts key.swapcase.capitalize + ' ' + gen_string(key)
-        end
-      end
+      a, str = some_preparations
+      loop_body(a, str)
     end
   end
 end
