15c7001fc09a4bbbcbca8998b6263c3ad39f6d3c
Pavel Zhachko <rammiel@yandex.ru>
2018-12-08 16:17:27 +0300

    new logic for lvl1 & lvl3, rubocop style check, some fixes, new bugs and etc

--- Pavel Zhachko/0/parser.rb
+++ Pavel Zhachko/0/parser.rb
@@ -11,73 +11,83 @@
 #   puts row.inspect
 # end
 
+SIMILAR_PRICE = 'For similar price you also can afford \''.freeze
+SIMILAR_STRING_NOT_FOUND = 'Similar prices can not be found in database.'.freeze
+
+require 'pry'
+require 'pry-coolline'
 require 'roo'
 # initialize in class
 class RooBookParser
   def initialize
     @xsl = Roo::Spreadsheet.open('data/Average_prices(serv)-10-2018.xlsx')
     @result = []
-    @similar_price = []
+    @similar_prices = []
     @similar_result = []
+    @s_p_result = []
   end
 
-  def output_parser(prices)
+  def search_price_by_name(input)
+    @result = @xsl.select { |elem| /^#{input.upcase}\b/.match?(elem[0]) }
+  end
+
+  def output_parser(prices, input)
     # lvl 1
+    return "\'#{input}\' can not be found in database." if prices.empty?
+
     prices.each do |elem|
       puts "#{elem[0]} is #{elem[14]} BYN in Minsk these days."
     end
     # lvl 2
+    # prices.each do |elem2|
+    # end
     # lvl 3
-    prices.each do |elem2|
-    end
   end
 
   def test_search; end
 
-  def search_similr_price(price, name)
+  def converting_similar_name(elem)
+    elem.split(' ').select { |el| el =~ /[[:upper:]]+/ }.join(' ').downcase
+  end
+
+  def search_similar_price_by_value(cell, price_value)
+    price_value.each do |sim_price|
+      @s_p_result = converting_similar_name(cell[0]) if cell.include?(sim_price)
+    end
+    @s_p_result
+  end
+
+  def search_similar_price_by_name(name)
+    @similar_prices = search_price_by_name(name).collect { |elem| elem[14] }
     @xsl.each do |elem2|
-      unless elem2[0] =~ /^#{name.upcase}\b/
-        price.each do |sim_price|
-          @similar_result << elem2[0]
-                             .split(' ')
-                             .select { |el| el =~ /[[:upper:]]+/ }
-                             .join(' ').downcase if elem2.include?(sim_price)
-        end
-      end
+      next unless /^#{name.upcase}\b/.match?(elem2[0])
+
+      @similar_result << search_similar_price_by_value(elem2, @similar_prices)
+      # @similar_prices.each do |sim_price|
+      #   @similar_result << converting_similar_name(elem2[0]) if elem2.include?(sim_price)
+      # end
     end
-    puts 'Similar prices can not be found in database.' if @similar_result.empty?
-    similar_output(@similar_result)
+    # p "#{@similar_result} v1"
+    @similar_result.empty? ? puts(SIMILAR_STRING_NOT_FOUND) : similar_prices_output(@similar_result)
   end
 
-  def similar_output(similar)
-    # p similar
-    # similar.map! do |s|
-    #   s.split(' ').select { |el| el =~ /[[:upper:]]+/ }.join(' ').downcase
-    # end
-    if similar.length == 2
-      puts "For similar price you also can afford \'#{similar[0]}\' and \'#{similar[1]}\'"
-    elsif similar.length >= 3
-      puts 'For similar price you also can afford \'' + similar[0..similar.length - 2].join('\', \'') +
-           + '\' and ' + "\'#{similar[-1]}\'"
-    else
-      puts 'Something wrong'
-    end
-  end
-
-  def search_price_by_name
-    puts 'What price are you looking for?'
-    input = gets.chomp
-    @xsl.each do |elem|
-      if elem[0] =~ /^#{input.upcase}\b/
-        @result << elem
-        @similar_price << elem[14]
+  # lvl 3
+  def similar_prices_output(similar)
+    out_string =
+      case similar.length
+      when 1
+        "\'#{similar[0]}\'."
+      when 2
+        "\'#{similar[0]}\' and \'#{similar[1]}\'"
+      when 3..Float::INFINITY
+        similar[0..similar.length - 2].join('\', \'') + '\' and ' + "\'#{similar[-1]}\'."
       end
-    end
-    puts "\'#{input}\' can not be found in database." if @result.empty?
-    output_parser(@result)
-    search_similr_price(@similar_price, input)
+    puts SIMILAR_PRICE + out_string
   end
 end
 
 r = RooBookParser.new
-r.search_price_by_name
+puts 'What price are you looking for?'
+input = gets.chomp
+r.output_parser(r.search_price_by_name(input), input)
+r.search_similar_price_by_name(input)
