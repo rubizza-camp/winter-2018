cfa1d30518833952e078219d036c12953c12b6f7
M1ha11 <tedpoolioo@gmail.com>
2018-12-09 18:31:23 +0300

    some fixes

--- Mihail Sokurenko/0/hw0.rb
+++ Mihail Sokurenko/0/hw0.rb
@@ -9,9 +9,9 @@ class Price
     @pr = 0
     @all = []
     @date = { 'январь': '01', 'февраль': '02', 'март': '03', 'апрель': '04',
-             'май': '05', 'июнь': '06', 'июль': '07', 'август': '08',
-             'сентябрь': '09', 'октябрь': '10', 'ноябрь': '11',
-             'декабрь': '12'}
+              'май': '05', 'июнь': '06', 'июль': '07', 'август': '08',
+              'сентябрь': '09', 'октябрь': '10', 'ноябрь': '11',
+              'декабрь': '12'}
   end
 
   def find
@@ -22,32 +22,30 @@ class Price
         if mas[0] != nil && mas[0] == @name.upcase || mas[0] =~ /#{@name.upcase}[.,) ]{1}/
           @l = i + 1
         end
-        if arr == 'г. Минск'
-          @k = j
-        end
+        (@k = j) if arr == 'г. Минск'
         @pr = @c.row(@l)[@k]
       end
     end
-    out()
+    out
   end
 
   def min
-  	min = @pr
+    min = @pr
     all_files
     @all.each do |f|
       if !f.nil?
         dat = get_date(f)
         old_price(f) ? coef = 0.0001 : coef = 1
         f.each_with_index do |_, i|
-          if f.row(i)[0] == @name.upcase || f.row(i)[0] =~ /#{@name.upcase}[.,) ]{1}/ 
-            if !f.row(i)[@k].nil? && (coef * f.row(i)[@k]) < min 
-              min=coef * f.row(i)[@k]
+          if f.row(i)[0] == @name.upcase || f.row(i)[0] =~ /#{@name.upcase}[.,) ]{1}/
+            if !f.row(i)[@k].nil? && (coef * f.row(i)[@k]) < min
+              min = coef * f.row(i)[@k]
               @min_dat = dat
             end
           end
         end
       end
-    end 
+    end
     min_out(min, @min_dat)
   end
 
@@ -59,15 +57,15 @@ class Price
         dat = get_date(f)
         old_price(f) ? coef = 0.0001 : coef = 1
         f.each_with_index do |_, i|
-          if f.row(i)[0] == @name.upcase || f.row(i)[0] =~ /#{@name.upcase}[.,) ]{1}/ 
-            if !f.row(i)[@k].nil? && (coef * f.row(i)[@k]) > max 
-              max=coef * f.row(i)[@k]
+          if f.row(i)[0] == @name.upcase || f.row(i)[0] =~ /#{@name.upcase}[.,) ]{1}/
+            if !f.row(i)[@k].nil? && (coef * f.row(i)[@k]) > max
+              max = coef * f.row(i)[@k]
               @max_dat = dat
             end
           end
         end
       end
-    end 
+    end
     max_out(max, @max_dat)
   end
 
@@ -87,24 +85,20 @@ class Price
   def get_date(file)
     dates = file.row(3)[0].split(' ')
     @date.each do |d, t|
-      if dates[1] == d.to_s
-        dates[1] = t
-      end
+      (dates[1] = t) if dates[1] == d.to_s
     end
     dates[1..2]
   end
 
   def old_price(file)
     date = get_date(file)
-    if date[1].to_i < 2017
-      true
-    end
+    (true) if date[1].to_i < 2017
   end
 
   def all_files
     Dir.foreach('./date').each do |file|
       case File.extname(file)
-      when '.xlsx' then 
+      when '.xlsx' then
         @all << Roo::Excelx.new("./date/#{file}")
       when '.xls' then
         @all << Roo::Excel.new("./date/#{file}")
@@ -113,7 +107,7 @@ class Price
     @all
   end
 
-  def out()
+  def out
     if @pr.nil? && @l.zero?
       puts "No #{@name} in table"
     else
@@ -135,14 +129,12 @@ class Price
   def similar_out(a)
     if a.empty?
       puts"No product with similar price"
-    else 
-      str = a.map{|r| "#{r.capitalize.gsub(/\s+/, " ").gsub(/\n/, " ")}"}.join(', ') 
-      puts "For similar price u can buy #{str}" 
+    else
+      str = a.map { |r| "#{r.to_s.capitalize.gsub(/\s+/, " ")}" }.join(', ')
+      puts "For similar price u can buy #{str}"
     end
   end
-
 end
 
 s = Price.new
 s.find
-
