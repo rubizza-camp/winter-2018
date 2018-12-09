83bd93cb2156abd38be2de641d47272bf47cc590
Makouchyk Vasili <vmakovchik@gmail.com>
2018-12-09 19:30:59 +0300

    adding changes and fixing style violations

--- winter-2018/Vasili Makouchyk/0/run.rb
+++ winter-2018/Vasili Makouchyk/0/run.rb
@@ -1,12 +1,24 @@
 require_relative 'menu.rb'
 loop do
   menu = Menu.new
+
   user_input = menu.ask_for_input
-  if user_input.casecmp('EXIT') == 0 then return end
+
+  return if user_input.casecmp('EXIT').zero?
+
   products = menu.find_product_in_current_file(user_input)
+
   next if products.empty?
+
   product = menu.choose_product_to_show(products)
+
   product_array = menu.find_product_in_previous_files(product)
+
   max_min_price = menu.find_max_min_price_at_date(product_array)
-  menu.view_products_found(product_array, max_min_price, menu.find_products_with_same_prise(product_array[0][1]))
+
+  menu.view_products_found(
+    product_array,
+    max_min_price,
+    menu.find_products_with_same_price(product_array[0][1])
+  )
 end
