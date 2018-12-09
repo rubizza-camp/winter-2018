d9e582a5ab3eb8a4c0edbccd7e60fc3de6fe83e4
Elizaveta Shved <elishved108@gmail.com>
2018-12-10 01:34:30 +0300

    Clear code

---
+++ Елизавета Швед/Елизавета Швед - 0/run.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'sqlite3'
+require './data_parser.rb'
+require './download_sheets.rb'
+require './db_requester.rb'
+
+class Waiter
+  def check_files
+    FileUtils.mkdir_p './data'
+    Dir['./data/*.*'].count < 100 ? DownloadSheets.new.perform_data : true
+  end
+
+  def check_data
+    File.new('test.db', 'a')
+    @db = SQLite3::Database.open 'test.db'
+    @db.execute 'CREATE TABLE IF NOT EXISTS Items(Id INTEGER PRIMARY KEY AUTOINCREMENT,
+      Name Text, Region TEXT, Price REAL, Date INTEGER )'
+    count = @db.execute 'SELECT count(*) FROM Items'
+    count[0][0].positive? ? true : DataParser.new.perform_data
+  end
+
+  def looping
+    check_files
+    check_data
+    loop do
+      puts 'What price are you looking for?'
+      DbRequester.new(gets.chomp).request
+    end
+  end
+end
+Waiter.new.looping
