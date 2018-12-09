13c86d9217fa4806d8a734f63e92203757c0e022
Mikhail Paulyshka <me@mixaill.tk>
2018-12-09 23:39:15 +0300

    implemented HW#0 levels 1,2,3

---
+++ Mikhail Paulyshka/0/data_downloader.rb
@@ -0,0 +1,87 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'fileutils'
+require 'uri'
+
+class DataDownloader
+  DIRECTORY = './data/'.freeze
+  URL = 'http://www.belstat.gov.by/ofitsialnaya-statistika/makroekonomika-i-okruzhayushchaya-sreda/tseny/operativnaya-informatsiya_4/srednie-tseny-na-potrebitelskie-tovary-i-uslugi-po-respublike-belarus/'.freeze
+
+  def self.download_file(uri, path)
+    dirname = File.dirname(path)
+    FileUtils.mkdir_p(dirname) unless File.directory?(dirname)
+
+    File.open(path, 'wb') do |file|
+      file.write(open(uri).read)
+    end
+  end
+
+  def self.filenames
+    Dir.entries(DIRECTORY).reject { |f| File.directory? f }.map { |f| DIRECTORY + f }
+  end
+
+  def self.download_data
+    page = Nokogiri::HTML(open(URL))
+
+    page.css('a[href*="xls"]').each do |link|
+      link = link['href']
+
+      # transform relative links to absolute ones
+      link.prepend('http://www.belstat.gov.by') if link.start_with? '/'
+      filename = link.split('/').last
+      fileext = File.extname(filename)
+      filetype = ''
+
+      # getting file type
+      if (filename.include? 'tov') || (filename.include? 'goods')
+        filetype = 'goods'
+      elsif filename.include? 'serv'
+        filetype = 'services'
+      else
+        filetype = 'unknown'
+      end
+
+      # getting file date
+      parsed = false
+      regex_result = filename.scan(/([0-9]{2})[-_]([0-9]{4})/) # 08_2019, 08-2018
+      if regex_result.any?
+        month = regex_result[0][0]
+        year = regex_result[0][1]
+        parsed = true
+      end
+
+      unless parsed
+        regex_result = filename.scan(/([0-9]{2})([0-9]{2})/) # 0917
+        if regex_result.any?
+          month = regex_result[0][0]
+          year = '20' + regex_result[0][1]
+          parsed = true
+        end
+      end
+
+      # little hack: file for 12_2014 does not containts year in link
+      unless parsed
+        if filename.include?('_cpi_12')
+          month = '12'
+          year = '2014'
+          parsed = true
+        end
+      end
+
+      unless parsed
+        puts "Failed to parse filename #{link}"
+        next
+      end
+
+      filename = "#{year}_#{month}_#{filetype}#{fileext}"
+
+      # perform file loading only if does not exists
+      download_path = DIRECTORY + filename
+      unless File.exist?(download_path)
+        print('.')
+        download_file(URI.escape(link).gsub('%25', '%'), download_path)
+      end
+    end
+  end
+end
---
+++ Mikhail Paulyshka/0/run.rb
@@ -0,0 +1,126 @@
+require 'rubygems'
+require 'roo'
+require 'roo-xls'
+
+require './data_downloader'
+
+PRICE_DELTA = 0.2
+NAME_COLUMN = 1
+PRICE_COLUMN = 7
+START_ROW = 9
+
+def load_files
+  goods = {}
+
+  # populate hash with data from files
+  files = DataDownloader.filenames
+  files.each do |file|
+    print('.')
+
+    # Files could be broken, so switch to next if it occurs
+    begin
+      spreadsheet = Roo::Spreadsheet.open(file, extension: File.extname(file))
+    rescue RuntimeError
+      next
+    end
+
+    # getting year and month
+    regex_result = file.scan(/([0-9]{4})_([0-9]{2})/) # 2018_02
+    if regex_result.none?
+      print("Invalid filename #{file}")
+      next
+    end
+    year = regex_result[0][0].to_i
+    month = regex_result[0][1]
+
+    # parsing data
+    goods_file = {}
+    (START_ROW..spreadsheet.last_row).each do |row|
+      # get name
+      name = spreadsheet.cell(row, 1).to_s.downcase.strip.gsub(/\s.+/, '')
+
+      # get price, if file <2017, then perform conversion from BYR to BYN
+      price = spreadsheet.cell(row, 7).to_f
+      price /= 10_000 if year < 2017
+
+      goods_file[name] = price
+    end
+
+    goods["#{year}.#{month}"] = goods_file
+  end
+
+  goods
+end
+
+def search(goods, item_name)
+  item_name_cap = item_name.downcase
+
+  prices_found = {}
+
+  goods = Hash[goods.sort_by { |key, _val| key }]
+  goods.each do |k, v|
+    prices_found[k] = v[item_name_cap] if v.key?(item_name_cap)
+  end
+
+  # level 1
+  # find if it exists
+  if prices_found.none?
+    print "'#{item_name}' can not be found in database.\n"
+    return
+  end
+
+  current_price = prices_found.to_a.last[1]
+  print "'#{item_name}' is #{current_price} BYN in Minsk these days.\n"
+
+  # level 2
+  # get min/max prices
+  max_price = 0.0
+  max_date = ''
+  min_price = Float::INFINITY
+  min_date = ''
+
+  prices_found.each do |k, v|
+    if v > max_price
+      max_date = k
+      max_price = v
+    end
+
+    if v < min_price
+      min_date = k
+      min_price = v
+    end
+  end
+  print "Lowest was on #{min_date} at price #{min_price} BYN\n"
+  print "Maximum was on #{max_date} at price #{max_price} BYN\n"
+
+  # level 3
+  # get products with similar price
+  similar_goods = []
+  goods.to_a.last[1].each do |k, v|
+    next if k == item_name_cap
+    similar_goods.push(k) if (current_price - v).abs < PRICE_DELTA
+  end
+
+  return if similar_goods.none?
+  print "For similar price you also can afford:\n"
+  similar_goods.each do |name|
+    print " * '#{name}'\n"
+  end
+end
+
+print "Downloading files. Please wait\n"
+DataDownloader.download_data
+
+print "\nParsing files. Please wait\n"
+goods = load_files
+
+print "\nReady. Print 'quit' to exit"
+loop do
+  print "\n\nWhat price are you looking for?\n"
+  print '> '
+
+  item_name = gets.chomp
+  exit if item_name == 'quit'
+
+  search(goods, item_name)
+end
