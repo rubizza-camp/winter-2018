e3efd24f91e56c9cf66d5a09bb494a802108907d
Alesia Yakutsionak <als6000@gmail.com>
2018-12-11 00:07:48 +0300

    Task 0: bugfixes, added level 3

--- Alesia Yakutsionak/0/run.rb
+++ Alesia Yakutsionak/0/run.rb
@@ -15,6 +15,7 @@ class ProductPrice
   end
 
   def update(price, date)
+    @last_price = price if date == '2018/10'
     if min_price > price
       @min_price = price
       @min_price_date = date
@@ -38,19 +39,21 @@ class PriceCollector
     collect_data
     found = search_product(ask_product)
     show_found_products(found)
-   # puts "Кол-во продуктов: #{products.size}"
   end
 
   private
 
   def collect_data
+    print 'Please wait'
     Dir['data/*'].each do |f|
       if f.end_with?('.xls')
         collect_from_xls(f)
       elsif f.end_with?('.xlsx')
         collect_from_xlsx(f)
       end
+      print '.'
     end
+    puts
   end
 
   def collect_from_xls(f)
@@ -62,7 +65,7 @@ class PriceCollector
     sheet.rows.each_with_index do |row,i|
       next if i < 8 || row[0].nil?
       name = clean_name(row[0])
-      price = row[6].to_f.round(2)
+      price = row[6]
       add_product(name, price, date)
     end
   end
@@ -75,12 +78,15 @@ class PriceCollector
     xlsx.sheet(0).each_row_streaming(offset: 7) do |row|
     name = clean_name(row[0].cell_value)
       next if name.nil?
-      price = row[6].cell_value.to_f.round(2)
+      price = row[6].cell_value
       add_product(name, price, date)
     end
   end
 
   def add_product(name, price, date)
+    price = price.to_f
+    price /= 10_000 if date < '2017/01'
+    price = price.round(2)
     existing_product = products[name]
     if existing_product
       existing_product.update(price, date)
@@ -110,24 +116,36 @@ class PriceCollector
 
   def search_product(user_product)
     products.keys.select do |name|
-      name.downcase.include?(user_product) && products[name].last_price
+      name.downcase.gsub(/[^(а-я)]/, ' ').split(/\s+/).include?(user_product) && products[name].last_price
     end
+
   end
 
   def show_found_products(found)
     found.each do |p|
       product_price = products[p]
-      puts "#{p} is #{product_price.last_price}BYN in Minsk these days."
-      puts "Lowest was on #{product_price.min_price_date} at price #{product_price.min_price}BYN"
-      puts "Maximum was on #{product_price.max_price_date} at price #{product_price.max_price}BYN"
+      puts "#{p} is #{product_price.last_price} BYN in Minsk these days."
+      puts "Lowest was on #{product_price.min_price_date} at price #{product_price.min_price} BYN"
+      puts "Maximum was on #{product_price.max_price_date} at price #{product_price.max_price} BYN"
+      same_price_products = find_same_price(found, product_price.last_price)
+      unless same_price_products.empty?
+        puts "For similar price you also can afford #{same_price_products.join(' and ')}."
+      end
+      puts
     end
+  end
+
+  def find_same_price(found, price)
+    same = []
+    products.each do |name, product_price|
+      if product_price.last_price && (product_price.last_price - price).abs < 0.03
+        same << name unless found.include?(name)
+        break if same.size == 2
+      end
+    end
+    same
   end
 end
 
 
 PriceCollector.new.start
-
-
-
-
-
