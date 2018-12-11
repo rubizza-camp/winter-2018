22c3d015834b42febe3481e39360db8e26b77475
nikitathebest <45689290+nikitathebest@users.noreply.github.com>
2018-12-11 14:00:56 +0200

    Update 0.rb

--- Nikita Mishuta/0/0.rb
+++ Nikita Mishuta/0/0.rb
@@ -1,13 +1,13 @@
 require 'roo'
 require 'roo-xls'
 puts 'What price are you looking for?'
-@name = gets.chomp.encode('UTF-8')
-@number = 0
+name = gets.chomp.encode('UTF-8')
+number = 0
 
 spread_sheet = Roo::Spreadsheet.open('./Data/Average.xlsx')
 
 (1..spread_sheet.last_row).each do |row|
-  next unless spread_sheet.cell(row, 1).to_s.start_with?(@name.upcase)
+  next unless spread_sheet.cell(row, 1).to_s.start_with?(name.upcase)
 
   item = spread_sheet.cell(row, 'A')
   price = spread_sheet.cell(row, 'O')
@@ -15,4 +15,4 @@ spread_sheet = Roo::Spreadsheet.open('./Data/Average.xlsx')
   @number += 1
 end
 
-puts "'#{@name}' can not be found in database" if @number.zero?
+puts "'#{name}' can not be found in database" if number.zero?
