9012a62918ebcc2730f8c7e232684ed174d281f4
Pavel Zhachko <rammiel@yandex.ru>
2018-12-16 00:15:44 +0300

    lvl2 logic done, templates for functions name, rubocop checking pass

--- Pavel Zhachko/0/parser.rb
+++ Pavel Zhachko/0/parser.rb
@@ -49,26 +49,57 @@ class RooBookParser
     array.join('/')
   end
 
-  def search_price_by_name(input_name, table = @actual_table)
-    table.select { |elem| regexp_template_for_item(input_name).match?(elem[0]) }
+  def search_price_by_name(name_of_item, table = @actual_table)
+    table.select { |elem| regexp_template_for_item(name_of_item).match?(elem[0]) || name_of_item == elem[0] }
   end
 
   def output_template(array_of_prices, name_of_item)
     puts("\'#{name_of_item}\' can not be found in database.") && return if array_of_prices.empty?
     array_of_prices.each do |elem|
       puts "#{name_of_item} is #{elem[MINSK_CONSTATN_CELL_COL]} BYN in these days."
+      lvl_2_out(elem[0])
+    end
+  end
+
+  def denomination_convertation(template)
+    # code for prices before 2017/01
+  end
+
+  def min_max_sort(array)
+    [
+      [array.min_by { |el| el[1] }[0], array.min_by { |el| el[1] }[1]],
+      [array.max_by { |el| el[1] }[0], array.max_by { |el| el[1] }[1]]
+    ]
+  end
+
+  def lvl_2(name)
+    arr = []
+    puts 'Put some data files if you wanna see prices by previous years' && return if @all_files.empty?
+    @all_files.sort.each do |table|
+      next if search_price_by_name(name, table[1]).empty?
+
+      arr << [table[0], search_price_by_name(name, table[1]).flatten[MINSK_CONSTATN_CELL_COL]]
+    end
+    min_max_sort(arr)
+  end
+
+  def lvl_2_out(name)
+    puts "Lowest was on #{lvl_2(name)[0][0]} at price #{lvl_2(name)[0][1]} BYN"
+    puts "Maximum was on #{lvl_2(name)[1][0]} at price #{lvl_2(name)[1][1]} BYN"
+  end
+
+  def lvl_1(input_name)
+    if collect_all_files.empty?
+      puts 'There is no file to check in data folder. So we take actual file from uri.'
+      search_price_by_name(input_name)
+    else
+      collect_all_files.each { |file| @all_files << [convert_date(convert_month_and_year(file)), file] }
+      search_price_by_name(input_name, @all_files.max_by { |el| el[0] }[1])
     end
   end
 
   def output_parser(input_name)
-    prices =
-      if collect_all_files.empty?
-        puts 'There is no file to check in data folder. So we take actual file from uri.'
-        search_price_by_name(input_name)
-      else
-        collect_all_files.each { |file| @all_files << [convert_date(convert_month_and_year(file)), file] }
-        search_price_by_name(input_name, @all_files.max_by { |el| el[0] }[1])
-      end
+    prices = lvl_1(input_name)
     output_template(prices, input_name)
   end
 end
