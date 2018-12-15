e4c659262dc1386d896cc869244006c071a4a41a
superpuper777 <superfevrik@gmail.com>
2018-12-15 19:02:33 +0300

    update level1.rb

--- Alena Zubritskaya/0/level1.rb
+++ Alena Zubritskaya/0/level1.rb
@@ -1,38 +1,24 @@
 require 'rubyXL'
 require 'pry'
-
+MNSK_COL = 14
 workbook = RubyXL::Parser.parse './data/Average_prices2018.xlsx'
 worksheets = workbook.worksheets
-puts "Found #{worksheets.count} worksheets"
+puts "Found #{worksheets.count} worksheet"
 
 worksheets.each do |worksheet_rows|
-  puts "Reading: #{worksheet_rows.sheet_name}"
   num_rows = 0
-
-  puts "What price are you looking for?"
+  puts 'What price are you looking for?'
   ans = gets.chomp
   ans.upcase!
-
-  worksheet.each do |row|
-    row_cells = row.cells.map{ |cell| cell.value }
-    num_rows += 1
-    puts row_cells.join " "
-  end
-
-
   worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
     item_name = row.cells[0].value
-    price = row.cells[14]&.value || "unknown"
-    if row.cells[0].value.include?(ans)
+    price = row.cells[MNSK_COL]&.value || "unknown"
+    if item_name.include?(ans)
     puts "#{item_name} is #{price} BYN in Minsk these days."
-    puts "#{date}"
-    binding.pry
-    num_rows+=1
+    num_rows += 1
   end
-  end
-  if
-    num_rows==0
+end
+  if num_rows==0
   puts  "#{ans} can not be found in database"
   end
-
 end
