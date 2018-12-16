59795d43ed88f59bf41ea6644b9c46179685ad21
superpuper777 <superfevrik@gmail.com>
2018-12-16 12:04:39 +0300

    update level1.rb

--- Alena Zubritskaya/0/level1.rb
+++ Alena Zubritskaya/0/level1.rb
@@ -13,12 +13,16 @@ worksheets.each do |worksheet_rows|
   worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
     item_name = row.cells[0].value
     price = row.cells[MNSK_COL]&.value || "unknown"
+
     if item_name.include?(ans)
+
     puts "#{item_name} is #{price} BYN in Minsk these days."
     num_rows += 1
   end
 end
-  if num_rows==0
-  puts  "#{ans} can not be found in database"
+  if
+
+    num_rows == 0
+    puts  "#{ans} can not be found in database"
   end
 end
