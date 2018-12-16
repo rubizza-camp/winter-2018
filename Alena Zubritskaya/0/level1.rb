0ff5dd40341270bffd615db1591bdc3bbc9383d6
superpuper777 <superfevrik@gmail.com>
2018-12-16 21:46:37 +0300

    corrected the bot's proposals accordingly in level1.rb

--- Alena Zubritskaya/0/level1.rb
+++ Alena Zubritskaya/0/level1.rb
@@ -8,13 +8,14 @@ worksheets.each do |worksheet_rows|
   puts 'What price are you looking for?'
   ans = gets.chomp
   ans.upcase!
+  
   worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
     item_name = row.cells[0].value
     price = row.cells[MNSK_COL]&.value || 'unknown'
-    next if !item_name.include?(ans)
+    next unless item_name.include?(ans)
 
     puts "#{item_name} is #{price} BYN in Minsk these days."
     num_rows += 1
-    end
-    puts  "#{ans} can not be found in database" if num_rows.zero?
+  end
+    puts "#{ans} can not be found in database" if num_rows.zero?
 end
