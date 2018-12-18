24226ef29470ca36b3521d37ad4256614ceaf67b
superpuper777 <superfevrik@gmail.com>
2018-12-18 16:25:55 +0300

    New level3.rb

--- Alena Zubritskaya/0/level3.rb
+++ Alena Zubritskaya/0/level3.rb
@@ -1,60 +1,73 @@
 require 'find'
+require 'pry'
 require 'rubyXL'
-#require 'pry'
 
-count = 0
-prices = []
-dates = []
-workbooks = []
-
-puts "What price are you looking for?"
+MNSK_COL = 14
+workbook = RubyXL::Parser.parse './data/Average_prices(serv)-10-2018.xlsx'
+worksheets = workbook.worksheets
+puts 'What price are you looking for?'
 ans = gets.chomp
 ans.upcase!
+worksheets.each do |worksheet_rows|
+  num_rows = 0
+  worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
+    item_name = row.cells[0].value
+    price = row.cells[MNSK_COL]&.value || 'unknown'
+    next unless item_name.include?(ans)
 
+    puts "#{item_name} is #{price} BYN in Minsk these days."
+    num_rows += 1
+  end
+    puts "#{ans} can not be found in database" if num_rows.zero?
+end
+prices = []
+dates = []
+similar_items = []
+workbooks = []
 Find.find('./data/') do |file|
-  if file =~ /\b.xlsx$\b/
+  next unless file =~ /\b.xlsx$\b/
+
+
     workbook = RubyXL::Parser.parse(file).worksheets
     workbooks << workbook
     workbook.each do |worksheet_rows|
-      if worksheet_rows[2][0].value.include?("2018")
-        current_year_prices = worksheet_rows
-      end
       num_rows = 0
       date = worksheet_rows[2][0].value
       worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
         item_name = row.cells[0].value
-        price = row.cells[14]&.value || 0
-        if row.cells[0].value.include?(ans)
+        price = row.cells[MNSK_COL]&.value || 0
+        next unless row.cells[0].value.include?(ans)
+
+
           dates << date
           prices << price
-          if price != 0 && !price.to_s.include?(".")
+          next unless price != 0 && !price.to_s.include?(".")
+
+
             new_price = (price.to_f/10000).round(2)
-            puts "#{item_name} is #{new_price} BYN #{date}"
             prices << new_price
-          else
-            puts "#{item_name} is #{price} BYN in Minsk these days"
           end
         end
       end
-      index_min = prices.each_with_index.min[1]
-      index_max = prices.select{|f| Float === f}.each_with_index.max[1]
-      similar_items = []
-      if worksheet_rows[2][0].value.include?("2018")
-        worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
-          item_price = row.cells[14]&.value || 0
-          if item_price > prices[index_min]  && item_price < prices[index_max]
-            similar_items << row.cells[0].value
-          end
-        end
+index_min = prices.each_with_index.min[1]
+index_max = prices.select{|f| Float === f}.each_with_index.max[1]
+puts "Lowest was on #{dates[index_min]} at price #{prices[index_min]} BYN"
+puts "Highest was on #{dates[index_max]} at price #{prices[index_max]} BYN"
+workbook = RubyXL::Parser.parse './data/Average_prices(serv)-10-2018.xlsx'
+worksheets = workbook.worksheets
+worksheets.each do |worksheet_rows|
+  next unless worksheet_rows[2][0].value.include?("2018")
+
+
+    num_rows = 0
+    worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
+      item_price = row.cells[MNSK_COL]&.value || 0
+      if item_price > prices[index_min]  && item_price < prices[index_max]
+        similar_items << row.cells[0].value
       end
-      puts "For similar price you also can afford:"
-      similar_items.each do |item|
-        puts "#{item}"
-      end
-      puts "Lowest was on #{dates[index_min]} at price #{prices[index_min]} BYN"
-      puts "Highest was on #{dates[index_max]} at price #{prices[index_max]} BYN"
     end
-    count += 1
-  end
+   end
+   puts "For similar price you also can afford:"
+   similar_items.first(3).each do |item|
+    puts "#{item}"
 end
-puts "#{count} files were found"
