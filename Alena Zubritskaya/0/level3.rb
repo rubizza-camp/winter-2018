7c4a5e009f3bb24f3528c8874cfb91f949f3d2bf
superpuper777 <superfevrik@gmail.com>
2018-12-19 14:55:21 +0300

    corrected the bot's proposals accordingly in level3.rb

--- Alena Zubritskaya/0/level3.rb
+++ Alena Zubritskaya/0/level3.rb
@@ -9,11 +9,10 @@ ans = gets.chomp
 ans.upcase!
 worksheets.each do |worksheet_rows|
   num_rows = 0
-  worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
+  worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each do |row|
     item_name = row.cells[0].value
     price = row.cells[MNSK_COL]&.value || 'unknown'
     next unless item_name.include?(ans)
-
 
     puts "#{item_name} is #{price} BYN in Minsk these days."
     num_rows += 1
@@ -26,28 +25,23 @@ similar_items = []
 workbooks = []
 Find.find('./data/') do |file|
   next unless file =~ /\b.xlsx$\b/
+  workbook = RubyXL::Parser.parse(file).worksheets
+  workbooks << workbook
+  workbook.each do |worksheet_rows|
+    num_rows = 0
+    date = worksheet_rows[2][0].value
+    worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
+      price = row.cells[MNSK_COL]&.value || 0
+      next unless row.cells[0].value.include?(ans)
 
+      dates << date
+      prices << price
+      next unless price != 0 && !price.to_s.include?('.')
 
-    workbook = RubyXL::Parser.parse(file).worksheets
-    workbooks << workbook
-    workbook.each do |worksheet_rows|
-      num_rows = 0
-      date = worksheet_rows[2][0].value
-      worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
-        item_name = row.cells[0].value
-        price = row.cells[MNSK_COL]&.value || 0
-        next unless row.cells[0].value.include?(ans)
-
-
-          dates << date
-          prices << price
-          next unless price != 0 && !price.to_s.include?(".")
-
-
-            new_price = (price.to_f / 10_000).round(2)
-            prices << new_price
-      end
+      new_price = (price.to_f / 10_000).round(2)
+      prices << new_price
     end
+  end
 end
 index_min = prices.each_with_index.min[1]
 index_max = prices.select { |f| Float === f }.each_with_index.max[1]
@@ -58,15 +52,14 @@ worksheets = workbook.worksheets
 worksheets.each do |worksheet_rows|
   next unless worksheet_rows[2][0].value.include?('2018')
 
-
   worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each do |row|
     item_price = row.cells[MNSK_COL]&.value || 0
-    if item_price > prices[index_min] && item_price < prices[index_max]
+    if item_price > prices[index_min] and item_price < prices[index_max]
       similar_items << row.cells[0].value
     end
   end
 end
 puts 'For similar price you also can afford:'
 similar_items.first(3).each do |item|
-  puts "#{item}"
+  p item.to_s
 end
