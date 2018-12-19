9b717785e3959be465e689fde76da14c4645a4cb
superpuper777 <superfevrik@gmail.com>
2018-12-19 14:54:59 +0300

    corrected the bot's proposals accordingly in level1.rb

--- Alena Zubritskaya/0/level1.rb
+++ Alena Zubritskaya/0/level1.rb
@@ -1,14 +1,13 @@
 require 'rubyXL'
 require 'pry'
 MNSK_COL = 14
-workbook = RubyXL::Parser.parse './data/Average_prices2018.xlsx'
+workbook = RubyXL::Parser.parse './data/Average_prices(serv)-10-2018.xlsx'
 worksheets = workbook.worksheets
 worksheets.each do |worksheet_rows|
   num_rows = 0
   puts 'What price are you looking for?'
   ans = gets.chomp
   ans.upcase!
-  
   worksheet_rows.select { |row| row&.cells&.at(0)&.value }.each_with_index do |row, index|
     item_name = row.cells[0].value
     price = row.cells[MNSK_COL]&.value || 'unknown'
@@ -17,5 +16,5 @@ worksheets.each do |worksheet_rows|
     puts "#{item_name} is #{price} BYN in Minsk these days."
     num_rows += 1
   end
-    puts "#{ans} can not be found in database" if num_rows.zero?
+  puts "#{ans} can not be found in database" if num_rows.zero?
 end
