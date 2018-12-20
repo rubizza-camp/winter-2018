206506b47c361f59bf63f5424bd36b116af7613f
M1ha11 <tedpoolioo@gmail.com>
2018-12-20 23:47:17 +0300

    rubocop fix

--- Mihail Sokurenko/0/hw0.rb
+++ Mihail Sokurenko/0/hw0.rb
@@ -7,11 +7,6 @@ class Price
     puts 'What price are u looking for?'
     @name = gets.chomp
     @pr = 0
-    @all = []
-    @date = { 'январь': '01', 'февраль': '02', 'март': '03', 'апрель': '04',
-              'май': '05', 'июнь': '06', 'июль': '07', 'август': '08',
-              'сентябрь': '09', 'октябрь': '10', 'ноябрь': '11',
-              'декабрь': '12' }
   end
 
   def find
@@ -19,7 +14,7 @@ class Price
     @l = 0
     @c.each_with_index do |mas, i|
       mas.each_with_index do |arr, j|
-        (@l = i + 1) if !mas[0].nil? && mas[0] == @name.upcase || mas[0] =~ /#{@name.upcase}[.,) ]{1}/
+        (@l = i + 1) if check(mas)
         (@k = j) if arr == 'г. Минск'
         @pr = @c.row(@l)[@k]
       end
@@ -27,68 +22,50 @@ class Price
     out
   end
 
-  def min
-    min = @pr
-    all_files
-    @all.each do |f|
-      unless f.nil?
-        dat = get_date(f)
-        old_price(f) ? coef = 0.0001 : coef = 1
-        f.each_with_index do |_, i|
-          if f.row(i)[0] == @name.upcase || f.row(i)[0] =~ /#{@name.upcase}[.,) ]{1}/
-            if !f.row(i)[@k].nil? && (coef * f.row(i)[@k]) < min
-              min = coef * f.row(i)[@k]
-              @min_dat = dat
-            end
-          end
-        end
-      end
-    end
-    min_out(min, @min_dat)
-  end
-
-  def max
-    max = @pr
-    all_files
-    @all.each do |f|
-      unless f.nil?
-        dat = get_date(f)
-        old_price(f) ? coef = 0.0001 : coef = 1
-        f.each_with_index do |_, i|
-          if f.row(i)[0] == @name.upcase || f.row(i)[0] =~ /#{@name.upcase}[.,) ]{1}/
-            if !f.row(i)[@k].nil? && (coef * f.row(i)[@k]) > max
-              max = coef * f.row(i)[@k]
-              @max_dat = dat
-            end
-          end
-        end
-      end
-    end
-    max_out(max, @max_dat)
+  def check(mas)
+    !mas[0].nil? && mas[0] == @name.upcase || mas[0] =~ /#{@name.upcase}[.,) ]{1}/
   end
 
   def similar
     a = []
     @c.each do |mas|
       mas.each do |arr|
-        (a << mas[0]) if arr == @pr && mas[0] !~ /#{@name.upcase}[.,) ]{1}/ && mas[0] != @name.upcase && !arr.nil?
+        (a << mas[0]) if arr == @pr && mas[0] !~ /#{@name.upcase}[.,) ]{1}/ && mas[0] != @name.upcase
       end
     end
-    a = a.uniq
-    similar_out(a)
+    similar_out(a.uniq)
   end
 
-  def get_date(file)
-    dates = file.row(3)[0].split(' ')
-    @date.each do |d, t|
-      (dates[1] = t) if dates[1] == d.to_s
+  def out
+    if @pr.nil? && @l.zero?
+      puts "No #{@name} in table"
+    else
+      puts "'#{@name.capitalize}' is #{@pr} BYN in Minsk on these days"
+      MinMaxPrice.new(@k, @pr, @name).min_max
+      similar
     end
-    dates[1..2]
   end
 
-  def old_price(file)
-    date = get_date(file)
-    true if date[1].to_i < 2017
+  def similar_out(arr)
+    if arr.empty?
+      puts 'No product with similar price'
+    else
+      str = arr.map { |r| r.to_s.capitalize.gsub(/\s+/, ' ') }.join(', ')
+      puts "For similar price u can buy #{str}"
+    end
+  end
+end
+
+class MinMaxPrice
+  def initialize(kol, price, name)
+    @k = kol
+    @pr = price
+    @name = name
+    @all = []
+    @date = { 'январь': '01', 'февраль': '02', 'март': '03', 'апрель': '04',
+              'май': '05', 'июнь': '06', 'июль': '07', 'август': '08',
+              'сентябрь': '09', 'октябрь': '10', 'ноябрь': '11',
+              'декабрь': '12' }
   end
 
   def all_files
@@ -103,15 +80,53 @@ class Price
     @all
   end
 
-  def out
-    if @pr.nil? && @l.zero?
-      puts "No #{@name} in table"
-    else
-      puts "'#{@name.capitalize}' is #{@pr} BYN in Minsk in these days"
-      min
-      max
-      similar
+  def minim(file, min, dat)
+    coef = old_price(file)
+    file.each_with_index do |_, i|
+      next unless file.row(i)[0] == @name.upcase || file.row(i)[0] =~ /#{@name.upcase}[.,) ]{1}/
+
+      min = checkmin(file.row(i)[@k], min, coef, dat)
     end
+    min
+  end
+
+  def maxim(file, max, dat)
+    coef = old_price(file)
+    file.each_with_index do |_, i|
+      next unless file.row(i)[0] == @name.upcase || file.row(i)[0] =~ /#{@name.upcase}[.,) ]{1}/
+
+      max = checkmax(file.row(i)[@k], max, coef, dat)
+    end
+    max
+  end
+
+  def checkmin(file, min, coef, dat)
+    if !file.nil? && (coef * file) < min
+      min = coef * file
+      @min_dat = dat
+    end
+    min
+  end
+
+  def checkmax(file, max, coef, dat)
+    if !file.nil? && (coef * file) > max
+      max = coef * file
+      @max_dat = dat
+    end
+    max
+  end
+
+  def get_date(file)
+    dates = file.row(3)[0].split(' ')
+    @date.each do |d, t|
+      (dates[1] = t) if dates[1] == d.to_s
+    end
+    dates[1..2]
+  end
+
+  def old_price(file)
+    date = get_date(file)
+    date[1].to_i < 2017 ? 0.0001 : 1
   end
 
   def min_out(min_v, min_dat)
@@ -122,13 +137,18 @@ class Price
     puts"Highest price was on #{max_dat[0]}/#{max_dat[1]} at #{max_v} BYN"
   end
 
-  def similar_out(arr)
-    if arr.empty?
-      puts 'No product with similar price'
-    else
-      str = arr.map { |r| r.to_s.capitalize.gsub(/\s+/, ' ') }.join(', ')
-      puts "For similar price u can buy #{str}"
+  def min_max
+    min = max = @pr
+    all_files
+    @all.each do |f|
+      next if f.nil?
+
+      dat = get_date(f)
+      min = minim(f, min, dat)
+      max = maxim(f, max, dat)
     end
+    min_out(min, @min_dat)
+    max_out(max, @max_dat)
   end
 end
 
