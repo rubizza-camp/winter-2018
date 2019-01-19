de227e3ce2fdbe21085828a749e40023ee223ded
Sikorskyyy <sikorskyalexandr08@gmail.com>
2019-01-20 00:19:24 +0300

    inital for task 1

---
+++ Sikorsky Alexandr/1/run.rb
@@ -0,0 +1,20 @@
+require 'telegram_bot'
+
+token = '723564519:AAHV5KL3h0pM0WZEv6_tcb5_5nbRdnJ0ToY'
+
+bot = TelegramBot.new(token: token)
+bot.get_updates(fail_silently: true) do |message|
+  puts "@#{message.from.username}: #{message.text}"
+  command = message.get_command_for(bot)
+
+  message.reply do |reply|
+    case command
+    when /hi/i
+      reply.text = "Hello, #{message.from.first_name}!"
+    else
+      reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means."
+    end
+    puts "sending #{reply.text.inspect} to @#{message.from.username}"
+    reply.send_with(bot)
+  end
+end
