39333306009f5230b7f72f220cee248139ddc49a
Vladislav Pozniak <vladislav.pozniak@tutby.com>
2019-01-24 17:31:03 +0300

    First implementation

--- Vladislav Pozniak/1/README.md
+++ Vladislav Pozniak/1/README.md
@@ -4,6 +4,6 @@ Scripts for the implementation of [Homework 1](https://github.com/rubizza-surviv
 
 ## Usage
 
-`bundle install`
-
-`ruby run.rb`
+Perform `bundle install`, install and start `redis` server.
+Put your bot's API token into the file `bot.token` in the script dir.
+Then run `ruby run.rb run` to start bot with already parsed puns (if exist), or `ruby run.rb clean_run` to start with recreated database.
--- Vladislav Pozniak/1/bot.rb
+++ Vladislav Pozniak/1/bot.rb
@@ -1,2 +1,33 @@
+# frozen_string_literal: true
+
 require 'telegram/bot'
 
+require_relative 'db.rb'
+
+# Telegram bot
+class Bot
+  def initialize(db)
+    @token = File.read('bot.token')
+    @bot = Telegram::Bot::Client.new(@token)
+    @db = db
+    puts 'Bot is started, enjoy!'
+    run
+  end
+
+  def reply(message)
+    case message.text
+    when 'Hey bot!'
+      @bot.api.send_message(chat_id: message.chat.id, text: 'Hey bro!')
+    when '/wordplay'
+      @bot.api.send_message(chat_id: message.chat.id, text: @db.random_select)
+    else
+      @bot.api.send_message(chat_id: message.chat.id, text: 'WAT?')
+    end
+  end
+
+  def run
+    @bot.listen do |message|
+      reply(message)
+    end
+  end
+end
