c3d77e58bbfa7a8f85d6b100f36f2f0093b0b6f0
Valentin <kopotchel@outlook.com>
2019-01-25 11:29:30 +0300

    Keys fixed

--- Valentine Kopotchel/1/bot_server.rb
+++ Valentine Kopotchel/1/bot_server.rb
@@ -6,6 +6,7 @@ redis = Redis.new(url: ENV['REDIS_URL'],
                   password: ENV['REDIS_PASSWORD']) # ENV was set in Heroku
 h = redis.get('db')
 h = JSON.parse h
+titles = h.keys
 Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN']) do |bot|
   bot.listen do |message|
     case message.text
@@ -20,7 +21,7 @@ Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN']) do |bot|
       bye = "Bye,#{message.from.first_name}"
       bot.api.send_message(chat_id: message.chat.id, text: bye)
     when '/wordplay'
-      title = h.keys.sample
+      title = titles.sample
       lyrics = h[title].sample
       bot.api.send_message(chat_id: message.chat.id,
                            text: "#{title}\n#{lyrics}")
