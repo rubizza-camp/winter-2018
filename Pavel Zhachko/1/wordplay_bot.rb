fe0c664644386804a874415519d1946fc0cb2f4d
Pavel Zhachko <rammiel@yandex.ru>
2019-01-29 02:51:58 +0300

    Playing with Redis, add Database, create classes for Bot and Db, reek & rubocop checks,add some bugs and bad code and etc.

--- Pavel Zhachko/1/wordplay_bot.rb
+++ Pavel Zhachko/1/wordplay_bot.rb
@@ -1,22 +1,45 @@
+# frozen_string_literal: true
+
 require 'telegram/bot'
+require_relative 'wordplay_db'
 
-token = '671502180:AAGTzsMZ3nzk_ZTPx0UqlG2m48JendDrGis'
+# Bot fot getting random wordplay from Database
+class Bot
+  def initialize
+    @token = '671502180:AAGTzsMZ3nzk_ZTPx0UqlG2m48JendDrGis'
+    @bot = Telegram::Bot::Client.new(@token)
+    @db = Database.new
+  end
 
-def wordplay
-  puts "Test"
-end
+  def wordplay
+    @db.wordplays_from_page('https://weirdblog.wordpress.com/2009/07/29/humorous-word-play-to-start-your-day')
+    @db.random_wordplay
+  end
 
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
+  def user_message(text)
+    @bot.api.send_message(chat_id: message.chat.id, text: text)
+  end
+
+  def reply(message)
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Sup?")
+      'Sup?'
     when 'Hey bot!'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hey bro!")
+      'Hey bro!'
     when '/wordplay'
-      wordplay
-    when '/end'
-      bot.api.send_message(chat_id: message.chat.id, text: "Cya, bro!") && return
+      "Here is your random wordplay:\n#{wordplay}"
+    else
+      'Huh?'
+    end
+  end
+
+  def run
+    @bot.listen do |message|
+      @bot.api.send_message(chat_id: message.chat.id, text: reply(message))
     end
   end
 end
+
+tg = Bot.new
+
+tg.run
