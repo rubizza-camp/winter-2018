df3d558d4d05ebc2a88feebb0e91c824871fb620
Pavel Zhachko <rammiel@yandex.ru>
2019-03-02 17:40:36 +0300

    Schema fixes

--- Pavel Zhachko/2/foodtracker_app/db/schema.rb
+++ Pavel Zhachko/2/foodtracker_app/db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_02_122934) do
+ActiveRecord::Schema.define(version: 2019_03_02_143834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dishes", force: :cascade do |t|
+    t.string "name"
+    t.integer "weight"
+    t.integer "calorie_value"
+    t.float "proteins"
+    t.float "carbohydrates"
+    t.float "fats"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
