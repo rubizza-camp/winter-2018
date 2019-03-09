a682c984c8bf1eb7dad6f8a9fe0356d7da048cca
Pavel Zhachko <rammiel@yandex.ru>
2019-03-09 23:24:04 +0300

    Huge part of code for views with bootstrap templates

--- Pavel Zhachko/2/foodtracker_app/app/assets/javascripts/application.js
+++ Pavel Zhachko/2/foodtracker_app/app/assets/javascripts/application.js
@@ -10,9 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 //= require Chart.bundle
 //= require chartkick
+//= require jquery3
+//= require popper
+//= require bootstrap
--- Pavel Zhachko/2/foodtracker_app/app/controllers/users_controller.rb
+++ Pavel Zhachko/2/foodtracker_app/app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+      @user = User.find params.require(:id)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
   end
 end
