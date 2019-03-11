47c4a54377c3b1a14429b19716ca1905a5810b19
Pavel Zhachko <rammiel@yandex.ru>
2019-03-11 03:06:59 +0300

    Add some bootstrap templates for dishes views. Fix input form in User registration. User model and controller chages.

--- Pavel Zhachko/2/foodtracker_app/app/controllers/users_controller.rb
+++ Pavel Zhachko/2/foodtracker_app/app/controllers/users_controller.rb
@@ -13,9 +13,25 @@ class UsersController < ApplicationController
       @user = User.find params.require(:id)
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
     redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
--- Pavel Zhachko/2/foodtracker_app/app/models/user.rb
+++ Pavel Zhachko/2/foodtracker_app/app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :current_password_is_correct,
+           if: :validate_password?, on: :update
+
+  attr_accessor :current_password
+
+  def current_password_is_correct
+  # For some stupid reason authenticate always returns false when called on self
+    if User.find(id).authenticate(current_password) == false
+      errors.add(:current_password, "is incorrect.")
+    end
+  end
+
+  def validate_password?
+   !password.blank?
+  end
+
 end
